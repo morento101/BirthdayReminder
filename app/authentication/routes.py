c9d42164c6b2f4d6b012bf479deeb5bde949ad09
Yaroslav Kovaliv <tlcees@gmail.com>
2022-11-05 14:11:46 +0200

    implemented route for refreshing access token, moved common auth logic to security module

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,17 +1,18 @@
-from datetime import datetime, timedelta
 from bson.objectid import ObjectId
 from fastapi import APIRouter, Response, status, Depends, HTTPException
 from app.authentication.oauth2 import AuthJWT
+from fastapi_jwt_auth.exceptions import MissingTokenError
 from app.core.database import user_collection
-from app.core.config import settings
 from app.authentication.schemas import (
     CreateUser, UserResponseSchema, LoginUser
 )
-from app.authentication.security import hash_password, verify_password
+from app.authentication.security import (
+    hash_password, verify_password, set_access_token_cookie, 
+    set_logged_in_cookie, set_refresh_token_cookie, generate_access_token,
+    generate_refresh_token
+)
 
 router = APIRouter(prefix="/auth", tags=["auth"])
-ACCESS_TOKEN_EXPIRES_IN = settings.ACCESS_TOKEN_EXPIRES_IN
-REFRESH_TOKEN_EXPIRES_IN = settings.REFRESH_TOKEN_EXPIRES_IN
 
 
 @router.post(
@@ -65,26 +66,45 @@ async def login(
     if not await verify_password(user_data.password, user_exists["password"]):
         raise credentials_exception
 
-    access_token = authorize.create_access_token(
-        subject=str(user_exists["_id"]),
-        expires_time=timedelta(minutes=ACCESS_TOKEN_EXPIRES_IN)
-    )
-    refresh_token = authorize.create_refresh_token(
-        subject=str(user_exists["_id"]),
-        expires_time=timedelta(minutes=REFRESH_TOKEN_EXPIRES_IN)
-    )
+    access_token = await generate_access_token(user_exists, authorize)
+    refresh_token = await generate_refresh_token(user_exists, authorize)
 
-    response.set_cookie(
-        'access_token', access_token, ACCESS_TOKEN_EXPIRES_IN * 60,
-        ACCESS_TOKEN_EXPIRES_IN * 60, httponly=True,
-    )
-    response.set_cookie(
-        'refresh_token', refresh_token, REFRESH_TOKEN_EXPIRES_IN * 60,
-        REFRESH_TOKEN_EXPIRES_IN * 60, httponly=True,
-    )
-    response.set_cookie(
-        'logged_in', 'True', ACCESS_TOKEN_EXPIRES_IN * 60,
-        ACCESS_TOKEN_EXPIRES_IN * 60,
-    )
+    await set_access_token_cookie(response, access_token)
+    await set_refresh_token_cookie(response, refresh_token)
+    await set_logged_in_cookie(response)
 
     return {'access_token': access_token, 'refresh_token': refresh_token}
+
+
+@router.get('/refresh')
+async def refresh_token(response: Response, authorize: AuthJWT = Depends()):
+    try:
+        authorize.jwt_refresh_token_required()
+        user_id = authorize.get_jwt_subject()
+
+        if not user_id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not refresh access token'
+            )
+
+        user = await user_collection.find_one({'_id': ObjectId(str(user_id))})
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='The user belonging to this token no logger exist'
+            )
+
+        access_token = await generate_access_token(user, authorize)
+
+    except MissingTokenError:
+        raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Please provide refresh token'
+            )
+
+    await set_access_token_cookie(response, access_token)
+    await set_logged_in_cookie(response)
+
+    return {'access_token': access_token}
--- app/authentication/security.py
+++ app/authentication/security.py
@@ -1,9 +1,13 @@
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
+from app.core.config import settings
+from datetime import timedelta
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+ACCESS_TOKEN_EXPIRES_IN = settings.ACCESS_TOKEN_EXPIRES_IN
+REFRESH_TOKEN_EXPIRES_IN = settings.REFRESH_TOKEN_EXPIRES_IN
 
 
 async def verify_password(plain_password, hashed_password):
@@ -12,3 +16,38 @@ async def verify_password(plain_password, hashed_password):
 
 async def hash_password(password):
     return pwd_context.hash(password)
+
+
+async def generate_access_token(user, authorize):
+    return authorize.create_access_token(
+        subject=str(user["_id"]),
+        expires_time=timedelta(minutes=ACCESS_TOKEN_EXPIRES_IN)
+    )
+
+
+async def generate_refresh_token(user, authorize):
+    return authorize.create_refresh_token(
+        subject=str(user["_id"]),
+        expires_time=timedelta(minutes=REFRESH_TOKEN_EXPIRES_IN)
+    )
+
+
+async def set_access_token_cookie(response, access_token):
+    response.set_cookie(
+        'access_token', access_token, ACCESS_TOKEN_EXPIRES_IN * 60,
+        ACCESS_TOKEN_EXPIRES_IN * 60, httponly=True,
+    )
+
+
+async def set_refresh_token_cookie(response, refresh_token):
+    response.set_cookie(
+        'refresh_token', refresh_token, REFRESH_TOKEN_EXPIRES_IN * 60,
+        REFRESH_TOKEN_EXPIRES_IN * 60, httponly=True,
+    )
+
+
+async def set_logged_in_cookie(response):
+    response.set_cookie(
+        'logged_in', 'True', ACCESS_TOKEN_EXPIRES_IN * 60, 
+        ACCESS_TOKEN_EXPIRES_IN * 60,
+    )
