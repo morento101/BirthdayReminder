af88b2a38771c01f9515916e60ddeb5b23f16ba0
Yaroslav Kovaliv <tlcees@gmail.com>
2023-01-09 17:53:14 +0200

    added task scheduling by solar noon

--- app/tasks/worker.py
+++ app/tasks/worker.py
@@ -1,6 +1,5 @@
-import time
-
 from celery import Celery
+from celery.schedules import solar
 
 from app.core.config import settings
 from app.email.email import send_birthday_reminder
@@ -8,6 +7,8 @@ from app.tasks.utils import (get_all_today_birthdays,
                              get_user_data_from_birthday)
 
 celery = Celery(__name__)
+celery.conf.timezone = 'Europe/Kiev'
+celery.conf.enable_utc = True
 celery.conf.broker_url = settings.CELERY_BROKER_URL
 celery.conf.result_backend = settings.CELERY_RESULT_BACKEND
 
@@ -15,6 +16,15 @@ celery.conf.result_backend = settings.CELERY_RESULT_BACKEND
 @celery.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(60, create_task.s())
+
+
+celery.conf.beat_schedule = {
+    'add-at-melbourne-sunset': {
+        'task': 'create_task',
+        'schedule': solar('solar_noon', 50.450001, 30.523333),
+        'args': (),
+    },
+}
 
 
 @celery.task(name="create_task")
